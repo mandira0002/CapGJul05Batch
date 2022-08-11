6716ddc4b46c9f399859dde2c63a7c5fb2818155
bhimatak <bhima.t@gmail.com>
2022-08-11 10:53:49 +0530

    Updated

---
+++ day02/src/p3.cpp
@@ -0,0 +1,34 @@
+/*
+
+fork() -> create child process with its own pid
+it will duplicate all the excution steps after it is called
+
+*/
+
+#include <myHeader.h>
+
+int main()
+{
+	int pid;
+
+	pid = fork();
+	// if(pid > 0)
+	// {
+	// 	cout<<"This is a parent process"<<endl;
+	// 	cout<<"pid from fork = "<<pid<<endl;
+	// 	cout<<"PID : "<<getpid()<<endl;
+	// 	cout<<"PPID : "<<getppid()<<endl;
+	// }
+
+	if(pid == 0)
+	{
+		cout<<"This is a child process"<<endl;
+		cout<<"pid from fork = "<<pid<<endl;
+		cout<<"PID : "<<getpid()<<endl;
+		cout<<"PPID : "<<getppid()<<endl;
+	}
+
+	cout<<"Hello World"<<endl;
+
+	return 0;
+}
