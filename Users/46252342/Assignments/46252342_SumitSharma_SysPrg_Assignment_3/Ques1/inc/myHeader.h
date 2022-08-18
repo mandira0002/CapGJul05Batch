a1f91970354f0fae4204c194b8f9f933f4eeb622
ssharm62 <sumitsharma0698@gmail.com>
2022-08-19 01:33:24 +0530

    Initial push - Assignment 3

---
+++ Users/46252342/Assignments/46252342_SumitSharma_SysPrg_Assignment_3/Ques1/inc/myHeader.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <signal.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <cstring>
+
+using namespace std;
+
