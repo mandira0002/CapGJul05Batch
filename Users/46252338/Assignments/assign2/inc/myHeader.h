dc1061d63fc904c15686b1dedd429762dee8f518
Harshini0431 <harshinee452@gmail.com>
2022-08-19 15:33:33 +0530

    Created assign2

---
+++ Users/46252338/Assignments/assign2/inc/myHeader.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include<iostream>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<unistd.h>
+#include<fstream>
+#include<string>
+#include<bits/stdc++.h>
+
+using namespace std;
+
